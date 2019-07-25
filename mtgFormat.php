44710d5e6cf90595279e040ced431e341acdf155
Mark Cheese <35660089+omfgitsmark@users.noreply.github.com>
2019-07-25 19:35:14 -0400

    Update mtgFormat.php

--- mtgFormat.php
+++ mtgFormat.php
@@ -1,9 +1,17 @@
 <?php
 
 function mtgFormat($str, $size = 12) {
-	$titletxt = ['T'=>'Tap', 'C'=>'Colorless', 'W'=>'White', 'G'=>'Green', 'R'=>'Red', 'U'=>'Blue', 'B'=>'Black', '0'=>'Zero', '1'=>'One Colorless', '2'=>'Two Colorless', '3'=>'Three Colorless', '4'=>'Four Colorless', '5'=>'Five Colorless', '6'=>'Six Colorless', '7'=>'Seven Colorless', '8'=>'Eight Colorless', '9'=>'Nine Colorless', '10'=>'Ten Colorless', '11'=>'Eleven Colorless', '12'=>'Twelve Colorless', '13'=>'Thirteen Colorless', '14'=>'Fourteen Colorless', '15'=>'Fifteen Colorless', '16'=>'Sixteen Colorless', '17'=>'Seventeen Colorless', '18'=>'Eighteen Colorless', '19'=>'Nineteen Colorless', '20'=>'Twenty Colorless', 'W/U'=>'White/Blue', 'W/B'=>'White/Black', 'U/R'=>'Blue/Red', 'U/B'=>'Blue/Black', 'R/W'=>'Red/White', 'R/G'=>'Red/Green', 'G/W'=>'Green/White', 'G/U'=>'Green/Blue', 'B/R'=>'Black/Red', 'B/G'=>'Black/Green', 'T'=>'Tap', '2B'=>'2 Colorless or 1 Black', '2G'=>'2 Colorless or 1 Green', '2R'=>'2 Colorless or 1 Red', '2U'=>'2 Colorless or 1 Blue', '2W'=>'2 Colorless or 1 White', 'BP'=>'Black/Phyrexian', 'GP'=>'Green/Phyrexian', 'RP'=>'Red/Phyrexian', 'UP'=>'Blue/Phyrexian', 'WP'=>'White/Phyrexian', 'S'=>'Snow-Covered', 'X'=>'X', 'Y'=>'Y', 'Z'=>'Z'];
-	// UN-SET TO ADD: 1/2, 1/2R, 1/2W, 100, 1000000, Infinity
-	
+	$titletxt = ['T'=>'Tap', 'C'=>'Colorless', 'W'=>'White', 'G'=>'Green', 'R'=>'Red', 'U'=>'Blue', 'B'=>'Black', '0'=>'Zero', 
+		     '1'=>'One Colorless', '2'=>'Two Colorless', '3'=>'Three Colorless', '4'=>'Four Colorless', '5'=>'Five Colorless', 
+		     '6'=>'Six Colorless', '7'=>'Seven Colorless', '8'=>'Eight Colorless', '9'=>'Nine Colorless', '10'=>'Ten Colorless', 
+		     '11'=>'Eleven Colorless', '12'=>'Twelve Colorless', '13'=>'Thirteen Colorless', '14'=>'Fourteen Colorless', 
+		     '15'=>'Fifteen Colorless', '16'=>'Sixteen Colorless', '17'=>'Seventeen Colorless', '18'=>'Eighteen Colorless', 
+		     '19'=>'Nineteen Colorless', '20'=>'Twenty Colorless', 'W/U'=>'White/Blue', 'W/B'=>'White/Black', 'U/R'=>'Blue/Red', 
+		     'U/B'=>'Blue/Black', 'R/W'=>'Red/White', 'R/G'=>'Red/Green', 'G/W'=>'Green/White', 'G/U'=>'Green/Blue', 
+		     'B/R'=>'Black/Red', 'B/G'=>'Black/Green', 'T'=>'Tap', '2B'=>'2 Colorless or 1 Black', '2G'=>'2 Colorless or 1 Green', 
+		     '2R'=>'2 Colorless or 1 Red', '2U'=>'2 Colorless or 1 Blue', '2W'=>'2 Colorless or 1 White', 'BP'=>'Black/Phyrexian', 
+		     'GP'=>'Green/Phyrexian', 'RP'=>'Red/Phyrexian', 'UP'=>'Blue/Phyrexian', 'WP'=>'White/Phyrexian', 'S'=>'Snow-Covered', 
+		     'X'=>'X', 'Y'=>'Y', 'Z'=>'Z'];
 	preg_match_all("/\{(.+?)\}/", $str, $matches, PREG_OFFSET_CAPTURE);
 	$returnstr = "";
 	$curpos = 0;
